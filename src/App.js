90fd5bda790094d7b94aa504aedce5f9bafe886c
Stefan . Gjorgjevikj <stefan.gjorgjevikj@musala.com>
2021-08-25 13:45:57 +0200

    added more testing examples

--- src/App.js
+++ src/App.js
@@ -1,103 +1,41 @@
-import React, { Component } from "react";
-import Header from "./component/header";
-import Headline from "./component/headline";
-import SharedButton from "./component/button";
-import ListItem from "./component/listItem";
-import { connect } from "react-redux";
-import { fetchPosts } from "./actions";
+import React, { useEffect } from "react";
 import "./app.scss";
-import Counter from "./component/counter";
 
-/* This const is not used within our app.
-   Although we are passing it to the Headline Component
-   it is only here as an exampleof testing PropTypes */
-const tempArr = [
-  {
-    fName: "Joe",
-    lName: "Bloggs",
-    email: "joebloggs@gmail.com",
-    age: 24,
-    onlineStatus: true,
-  },
-];
+import useBooleanToggle from "./hooks/useBooleanToggle";
+import useRest from "./hooks/useRest";
 
-const initialState = {
-  hideBtn: false,
-};
+function App() {
+  const { status, handleStatusChange } = useBooleanToggle(false);
+  const { data, error, doFetch } = useRest();
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      ...initialState,
-    };
-    this.fetch = this.fetch.bind(this);
-  }
+  useEffect(() => {
+    if (!data) {
+      return;
+    }
+    console.log(data);
+    debugger;
+  }, [data]);
 
-  fetch() {
-    this.props.fetchPosts();
-    this.exampleMethod_updatesState();
-  }
+  useEffect(() => {
+    if (!error) {
+      return;
+    }
+    console.log(error);
+    debugger;
+  }, [error]);
 
-  exampleMethod_updatesState() {
-    const { hideBtn } = this.state;
-    this.setState({
-      hideBtn: !hideBtn,
-    });
-  }
+  const onClick = () => doFetch("https://swapi.dev/api/planets/1/");
 
-  exampleMethod_returnsAValue(number) {
-    return number + 1;
-  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button onClick={handleStatusChange}>Show Message</button>
+        {status && <h1>Hello friends!</h1>}
 
-  handleCountChange(count) {
-    console.log(count);
-  }
-
-  render() {
-    const { posts } = this.props;
-    const { hideBtn } = this.state;
-
-    const configButton = {
-      buttonText: "Get posts",
-      emitEvent: this.fetch,
-    };
-
-    return (
-      <div className="App" data-test="appComponent">
-        <Header />
-        <section className="main">
-          <Counter onCountChange={() => this.handleCountChange()} />
-          <Headline
-            header="Posts"
-            desc="Click the button to render posts!"
-            tempArr={tempArr}
-            hideText={hideBtn}
-          />
-          {!hideBtn && <SharedButton {...configButton} />}
-
-          {posts.length > 0 && (
-            <div>
-              {posts.map((post, index) => {
-                const { title, body } = post;
-                const configListItem = {
-                  title,
-                  desc: body,
-                };
-                return <ListItem key={index} {...configListItem} />;
-              })}
-            </div>
-          )}
-        </section>
-      </div>
-    );
-  }
+        <button onClick={onClick}>get data</button>
+      </header>
+    </div>
+  );
 }
 
-const mapStateToProps = (state) => {
-  return {
-    posts: state.posts,
-  };
-};
-
-export default connect(mapStateToProps, { fetchPosts })(App);
+export default App;
