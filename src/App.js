42a36291ed45b725c9ef96d476374e14f4992281
Stefan . Gjorgjevikj <stefan.gjorgjevikj@musala.com>
2021-08-25 14:02:34 +0200

    added new files for testing

--- src/App.js
+++ src/App.js
@@ -1,39 +1,15 @@
 import React, { useEffect } from "react";
 import "./app.scss";
-
-import useBooleanToggle from "./hooks/useBooleanToggle";
-import useRest from "./hooks/useRest";
+import { useCounter } from "./hooks/useCounter";
 
 function App() {
-  const { status, handleStatusChange } = useBooleanToggle(false);
-  const { data, error, doFetch } = useRest();
-
-  useEffect(() => {
-    if (!data) {
-      return;
-    }
-    console.log(data);
-    debugger;
-  }, [data]);
-
-  useEffect(() => {
-    if (!error) {
-      return;
-    }
-    console.log(error);
-    debugger;
-  }, [error]);
-
-  const onClick = () => doFetch("https://swapi.dev/api/planets/1/");
+  const { count, increment, decrement } = useCounter();
 
   return (
     <div className="App">
-      <header className="App-header">
-        <button onClick={handleStatusChange}>Show Message</button>
-        {status && <h1>Hello friends!</h1>}
-
-        <button onClick={onClick}>get data</button>
-      </header>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Up</button>
+      <button onClick={decrement}>Down</button>
     </div>
   );
 }
