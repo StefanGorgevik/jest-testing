4c59b15bed88538b17a091ec732d6499e31644ad
Stefan . Gjorgjevikj <stefan.gjorgjevikj@musala.com>
2021-08-27 18:42:41 +0200

    added event simulation for input tet

--- testingcourse/src/Joto/Input/Input.test.js
+++ testingcourse/src/Joto/Input/Input.test.js
@@ -12,3 +12,20 @@ test("renders without err", () => {
   const input = findByTestAttr(wrapper, "component-input");
   expect(input.length).toBe(1);
 });
+
+describe("state controlled input field", () => {
+  test("state updates with value of input box upon change", () => {
+    const mockSetCurrentGuess = jest.fn();
+    React.useState = jest.fn(() => {
+      ["", mockSetCurrentGuess];
+    });
+    const wrapper = setup();
+
+    const inputBox = findByTestAttr(wrapper, "input-box");
+
+    const mockEvent = { target: { value: "train" } };
+    inputBox.simulate("change", mockEvent);
+
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith("train");
+  });
+});
--- testingcourse/src/Joto/Input/Input.tsx
+++ testingcourse/src/Joto/Input/Input.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
 interface InputProps {
-
+    secretWord: boolean
 }
+const Input: React.FC<InputProps> = ({ secretWord }) => {
+    const [currentGuess, setCurrentGuess] = React.useState('');
 
-const Input: React.FC<InputProps> = () => {
-    return (<div data-test='component-input'></div>)
+    return (<div data-test='component-input'>
+        <form>
+            <input type='text' data-test='input-box' placeholder='Enter guess' value={currentGuess} onChange={(e) => setCurrentGuess(e.target.value)} />
+            <button type='submit' data-test='submit-button'>
+                Submit
+            </button>
+        </form>
+    </div>)
 }
 
 export default Input;
