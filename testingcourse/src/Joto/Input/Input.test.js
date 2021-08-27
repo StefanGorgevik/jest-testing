a743fd3f7b52f2409df47aa077a83bc2f5df0bb8
Stefan . Gjorgjevikj <stefan.gjorgjevikj@musala.com>
2021-08-27 19:09:11 +0200

    finished input test

--- testingcourse/src/Joto/Input/Input.test.js
+++ testingcourse/src/Joto/Input/Input.test.js
@@ -3,29 +3,89 @@ import { shallow } from "enzyme";
 import { findByTestAttr } from "../test/testUtils";
 import Input from "./Input";
 
-const setup = () => {
-  return shallow(<Input />);
+const mockSetCurrentGuess = jest.fn();
+
+jest.mock("react", () => ({
+  ...jest.requireActual("react"),
+  useState: (initState) => [initState, mockSetCurrentGuess],
+}));
+
+const setup = (success = false, secretWord = "party") => {
+  return shallow(<Input secretWord={secretWord} success={success} />);
 };
 
-test("renders without err", () => {
-  const wrapper = setup();
-  const input = findByTestAttr(wrapper, "component-input");
-  expect(input.length).toBe(1);
+describe("render", () => {
+  describe("success is true", () => {
+    let wrapper;
+    beforeEach(() => {
+      wrapper = setup(true);
+    });
+
+    test("renders without err", () => {
+      const input = findByTestAttr(wrapper, "component-input");
+      expect(input.length).toBe(1);
+    });
+
+    test("input box does not show", () => {
+      const input = findByTestAttr(wrapper, "input-box");
+      expect(input.exists()).toBe(false);
+    });
+    test("submitButton does not show", () => {
+      const submitButton = findByTestAttr(wrapper, "submit-button");
+      expect(submitButton.exists()).toBe(false);
+    });
+  });
+
+  describe("success is false", () => {
+    let wrapper;
+    beforeEach(() => {
+      wrapper = setup(false);
+    });
+
+    test("input renders without err", () => {
+      const input = findByTestAttr(wrapper, "component-input");
+      expect(input.length).toBe(1);
+    });
+
+    test("input box shows", () => {
+      const input = findByTestAttr(wrapper, "input-box");
+      expect(input.exists()).toBe(true);
+    });
+    test("submitButton shows", () => {
+      const submitButton = findByTestAttr(wrapper, "submit-button");
+      expect(submitButton.exists()).toBe(true);
+    });
+  });
 });
 
 describe("state controlled input field", () => {
-  test("state updates with value of input box upon change", () => {
-    const mockSetCurrentGuess = jest.fn();
-    React.useState = jest.fn(() => {
-      ["", mockSetCurrentGuess];
-    });
-    const wrapper = setup();
+  let wrapper;
+  //let originalUseState;
+  //const mockSetCurrentGuess = jest.fn();
+  beforeEach(() => {
+    //mockSetCurrentGuess.mockClear();
+    // originalUseState = React.useState;
+    // React.useState = jest.fnc(() => ['', mockSetCurrentGuess])
+    wrapper = setup();
+  });
 
+  // afterEach(() => {
+  //     React.useState = originalUseState;
+  // })
+
+  test("state updates with value of input box upon change", () => {
     const inputBox = findByTestAttr(wrapper, "input-box");
 
     const mockEvent = { target: { value: "train" } };
     inputBox.simulate("change", mockEvent);
 
     expect(mockSetCurrentGuess).toHaveBeenCalledWith("train");
+  });
+
+  test("field is cleared upon submit button click", () => {
+    const submitButton = findByTestAttr(wrapper, "submit-button");
+
+    submitButton.simulate("click", { preventDefault() {} });
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith("");
   });
 });
--- testingcourse/src/Joto/Input/Input.tsx
+++ testingcourse/src/Joto/Input/Input.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 interface InputProps {
+    success: boolean,
     secretWord: boolean
 }
-const Input: React.FC<InputProps> = ({ secretWord }) => {
-    const [currentGuess, setCurrentGuess] = React.useState('');
+const Input: React.FC<InputProps> = ({ success, secretWord }) => {
+    const [currentGuess, setCurrentGuess] = useState('');
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        setCurrentGuess('');
+    }
+    if (success) {
+        return (<div data-test='component-input'></div>)
+    }
 
     return (<div data-test='component-input'>
         <form>
             <input type='text' data-test='input-box' placeholder='Enter guess' value={currentGuess} onChange={(e) => setCurrentGuess(e.target.value)} />
-            <button type='submit' data-test='submit-button'>
+            <button type='submit' data-test='submit-button' onClick={submitHandler}>
                 Submit
             </button>
         </form>
